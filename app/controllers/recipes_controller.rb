da0511a8b325a735afd401735db488a8190ef7e8
danielmekuriaw <daniel.mekuriaw16@gmail.com>
2021-07-18 20:39:05 -0500

    Basic nested forms lab

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,23 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build
+    #@recipe.ingredients.build
+    #@recipe.ingredients.build(quantity: "1 cup")
+    #@recipe.ingredients.build(quantity: "1 tablespoon")
   end
 
   def create
+    @recipe = Recipe.create(recipe_params)
+    puts @recipe
+    puts recipe_params
+
+    redirect_to recipe_path(@recipe)
   end
+
+  private
+
+  def recipe_params
+      params.require(:recipe).permit(:title, ingredients: [:name, :quantity])
+  end 
 end
